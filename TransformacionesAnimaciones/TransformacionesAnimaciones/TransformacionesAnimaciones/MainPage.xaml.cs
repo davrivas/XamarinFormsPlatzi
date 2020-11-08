2d53c20ca24e9ab940eadb097802a4baaf7afb95
David Rivas <david.rivas@pevaar.com>
2020-11-08 02:54:21 -0500

    TransformacionesAnimaciones

---
+++ TransformacionesAnimaciones/TransformacionesAnimaciones/TransformacionesAnimaciones/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TransformacionesAnimaciones
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnClicked(object sender, EventArgs e)
+        {
+            await button1.ScaleTo(1.5, 2000, Easing.CubicInOut);
+            await button1.ScaleTo(1, 2000, Easing.SpringIn);
+        }
+    }
+}
