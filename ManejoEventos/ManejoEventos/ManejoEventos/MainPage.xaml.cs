0b87e90e00b8e88ae74e40a1ee63e44b1c95de54
David Rivas <david.rivas@pevaar.com>
2020-11-08 02:50:15 -0500

    ManejoEventos

---
+++ ManejoEventos/ManejoEventos/ManejoEventos/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ManejoEventos
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            boton.Clicked += (s, a) => etiqueta.Text = "Hola " + DateTime.Now.ToString();
+        }
+    }
+}
