cc6f36d1fd7064e76074f13e02254491cb8b9456
David Rivas <david.rivas@pevaar.com>
2020-11-08 02:50:50 -0500

    LenguajeXaml

---
+++ LenguajeXaml/LenguajeXaml/LenguajeXaml/Literals.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LenguajeXaml
+{
+    internal class Literals
+    {
+        public static string Message = "Hola a todos!";
+    }
+}
