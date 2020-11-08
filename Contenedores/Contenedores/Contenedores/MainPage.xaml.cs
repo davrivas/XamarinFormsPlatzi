a28a07c04530c20cf4358a80ac2f9b76c148addb
David Rivas <david.rivas@pevaar.com>
2020-11-08 02:52:32 -0500

    Contenedores

---
+++ Contenedores/Contenedores/Contenedores/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Contenedores
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            var box = sender as BoxView;
+
+            box.Color = box.Color == Color.LightGray
+                ? Color.Red
+                : Color.LightGray;
+        }
+    }
+}
