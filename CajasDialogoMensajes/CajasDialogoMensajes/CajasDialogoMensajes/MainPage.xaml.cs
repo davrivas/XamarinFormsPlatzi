55f434ce08a581faaa13e4ffe44d92d258a62e23
David Rivas <david.rivas@pevaar.com>
2020-11-08 02:55:11 -0500

    CajasDialogoMensajes

---
+++ CajasDialogoMensajes/CajasDialogoMensajes/CajasDialogoMensajes/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CajasDialogoMensajes
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            MessagingCenter.Subscribe<Page2>(this, "Hola", async s =>
+            {
+                await DisplayAlert("Saludo", "Hola!!!", "Ok");
+            });
+        }
+
+        private async void OnClicked(object sender, EventArgs e)
+        {
+            //var result = await DisplayActionSheet("Opciones", "Ok", null, 
+            //    new[] { "Colombia", "México", "Costa Rica", "Perú" });
+
+            //await DisplayAlert("Saludo", "Hola " + result, "Aceptar");
+
+            await Navigation.PushAsync(new Page2());
+        }
+    }
+}
