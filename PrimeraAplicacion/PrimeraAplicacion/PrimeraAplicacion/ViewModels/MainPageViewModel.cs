953416d871e96a4b8132a7dbf1163d0f07316ac7
David Rivas <david.rivas@pevaar.com>
2020-11-08 02:48:34 -0500

    PrimeraAplicacion

---
+++ PrimeraAplicacion/PrimeraAplicacion/PrimeraAplicacion/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,42 @@
+﻿using PrimeraAplicacion.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace PrimeraAplicacion.ViewModels
+{
+    public class MainPageViewModel : NotificationObject
+    {
+        private ObservableCollection<Survey> _survey;
+
+        public ObservableCollection<Survey> Surveys {
+            get { return _survey; }
+            set { 
+                _survey = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand AddCommand { get; set; }
+
+        public MainPageViewModel()
+        {
+            Surveys = new ObservableCollection<Survey>();
+
+            AddCommand = new Command(AddCommandExecute);
+
+            MessagingCenter.Subscribe<SurveyDetailsViewModel, Survey>(this, "SaveSurvey", (a, s) =>
+            {
+                Surveys.Add(s);
+            });
+        }
+
+        private void AddCommandExecute()
+        {
+            MessagingCenter.Send(this, "AddSurvey");
+        }
+    }
+}
---
+++ PrimeraAplicacion/PrimeraAplicacion/PrimeraAplicacion/ViewModels/SurveyDetailsViewModel.cs
@@ -0,0 +1,50 @@
+﻿using PrimeraAplicacion.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace PrimeraAplicacion.ViewModels
+{
+    public class SurveyDetailsViewModel : NotificationObject
+    {
+        private string _name;
+
+        public string Name {
+            get { return _name; }
+            set { 
+                _name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _favoriteFood;
+
+        public string FavoriteFood {
+            get { return _favoriteFood; }
+            set { 
+                _favoriteFood = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand SaveCommand { get; }
+
+
+        public SurveyDetailsViewModel()
+        {
+            SaveCommand = new Command(SaveCommandExecute);
+        }
+
+        private void SaveCommandExecute()
+        {
+            var newSurvey = new Survey
+            {
+                Name = Name,
+                FavoriteFood = FavoriteFood
+            };
+            MessagingCenter.Send(this, "SaveSurvey", newSurvey);
+        }
+    }
+}
